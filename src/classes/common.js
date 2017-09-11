f44c3440e7e447ea2ac919ce144448c3da171efb
Dimitri Do Bairro <dimitri.dobairro@clicrdv.com>
2017-09-11 17:17:44 +0200

    feat(classes/helpers.js) helpers class renamed, code refactored

---
+++ src/classes/common.js
@@ -0,0 +1,18 @@
+// @flow
+
+export class Common {
+
+  allowPrefix: string;
+
+  makeArray(arr: mixed) {
+    return Array.isArray(arr) ? arr : [arr];
+  }
+
+  allowsBucket(role: string) {
+    return this.allowPrefix + role;
+  }
+
+  keyFromAllowsBucket(str: string) {
+    return str.replace(`/^${this.allowPrefix}/`, '');
+  }
+}
