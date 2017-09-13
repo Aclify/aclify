103f7f37ea28e21bc9ba90a12d6ff1e293b37ee2
Dimitri <contact@dimsolution.com>
2017-09-13 17:42:02 +0200

    chore(__tests__/index.test.js) allows tests created

---
+++ __tests__/index.test.js
@@ -0,0 +1,27 @@
+import Acl from '../src/classes/acl';
+import Memory from '../src/stores/memory';
+
+const store = new Memory();
+
+describe('Allows', () => {
+  it('guest to view blogs', () => {
+    const acl = new Acl();
+    acl.allow('guest', 'blogs', 'view', (err) => {
+      expect(!err);
+    });
+  });
+
+  it('guest to view forums', () => {
+    const acl = new Acl(store);
+    acl.allow('guest', 'forums', 'view', (err) => {
+      expect(!err);
+    });
+  });
+
+  it('member to view/edit/delete blogs', () => {
+    const acl = new Acl(store);
+    acl.allow('member', 'blogs', ['edit', 'view', 'delete'], (err) => {
+      expect(!err);
+    });
+  });
+});
