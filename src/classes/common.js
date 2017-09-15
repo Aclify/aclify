7f319d4773c1597327db6c6c51563b4bbeec47e3
Dimitri Do Bairro <dimitri.dobairro@clicrdv.com>
2017-09-15 12:19:56 +0200

    feat(classes/common.js) allowPrefix is auto set

--- src/classes/common.js
+++ src/classes/common.js
@@ -1,10 +1,12 @@
 // @flow
-
 export default class Common {
-
   allowPrefix: string;
 
-  makeArray(arr: mixed) {
+  constructor() {
+    this.allowPrefix = 'allows_';
+  }
+
+  makeArray(arr: mixed): Array<any> {
     return Array.isArray(arr) ? arr : [arr];
   }
 
