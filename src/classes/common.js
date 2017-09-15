e5663f8d7ec77d9ca51526ad4cf6eac325acb2ab
Dimitri Do Bairro <dimitri.dobairro@clicrdv.com>
2017-09-15 15:42:02 +0200

    feat(class/common.js) prefix attribute updated

--- src/classes/common.js
+++ src/classes/common.js
@@ -1,20 +1,14 @@
 // @flow
 export default class Common {
-  allowPrefix: string;
-
-  constructor() {
-    this.allowPrefix = 'allows_';
-  }
-
   makeArray(arr: mixed): Array<any> {
     return Array.isArray(arr) ? arr : [arr];
   }
 
-  allowsBucket(role: string) {
-    return this.allowPrefix + role;
+  allowsBucket(role: string): string {
+    return 'allows_' + role;
   }
 
-  keyFromAllowsBucket(str: string) {
-    return str.replace(`/^${this.allowPrefix}/`, '');
+  keyFromAllowsBucket(str: string): string {
+    return str.replace(/^allows_/, '');
   }
 }
