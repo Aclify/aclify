769f919ba86732f0cbab2968321c4c9d2f4d03a7
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2017-10-03 14:22:46 +0200

    chore(__tests__/stores.js) run tests for redis store

--- __tests__/stores.js
+++ __tests__/stores.js
@@ -1,13 +1,8 @@
 // @flow
-import RedisMock from 'redis';
+import Redis from 'redis';
 import Tests from './tests';
 import Memory from '../src/stores/memory';
 import RedisStore from '../src/stores/redis';
 
-// describe('Memory', () => {
-//   Tests(new Memory());
-// });
-
-describe('Redis', () => {
-  Tests(new RedisStore(RedisMock.createClient()));
-});
+describe('Memory', () => Tests(new Memory()));
+describe('Redis', () => Tests(new RedisStore(Redis.createClient())));
