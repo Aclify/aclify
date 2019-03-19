c7ce2c023bda494fd7ec08347b538e62c0742907
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2019-03-19 15:56:27 +0100

    feat(classes/acl.ts) constructor + allow method rewrite

--- src/classes/acl.ts
+++ src/classes/acl.ts
@@ -1,12 +1,15 @@
-import { IOptions } from "../interfaces/IAcl";
+import { IOptions, IPermissions, IResources, IRoles } from "../interfaces/IAcl";
 import * as _ from "lodash";
 import { IStore } from "../interfaces/IStore";
+import { Common } from "./common";
 
-export class Acl {
-  readonly options;
-  readonly store;
+export class Acl extends Common {
+  readonly options: IOptions;
+  readonly store: IStore;
 
   constructor(store: IStore, options?: IOptions) {
+    super();
+    this.store = store;
     this.options = _.extend({
       buckets: {
         meta: 'meta',
@@ -17,5 +20,34 @@ export class Acl {
         users: 'users',
       },
     }, options);
+  }
+
+  /**
+   * @description
+   * @param roles
+   * @param resources
+   * @param permissions
+   * @return Promise<void>
+   */
+  async allow(roles: IRoles, resources: IResources, permissions: IPermissions): Promise<void> {
+    const [rolesParam, resourcesParam] = await Promise.all([
+      Common.makeArray(roles),
+      Common.makeArray(resources),
+    ]);
+
+    this.store.begin();
+    await this.store.add(this.options.buckets.meta, 'roles', rolesParam);
+
+    resourcesParam.forEach((resource) => {
+      rolesParam.forEach(async (role) => {
+        await this.store.add(this.allowsBucket(resource), role, permissions);
+      });
+    });
+
+    rolesParam.forEach(async (role) => {
+      await this.store.add(this.options.buckets.resources, role, resourcesParam);
+    });
+
+    return this.store.end();
   }
 }
