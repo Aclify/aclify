0eb121037ea70a43236018e2e44c6da5f0ae65b3
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2019-03-20 09:59:09 +0100

    feat(classes/acl.ts) file created

--- src/classes/acl.ts
+++ src/classes/acl.ts
@@ -1,11 +1,11 @@
-import { IOptions, IPermissions, IResources, IRoles } from "../interfaces/IAcl";
+import { IOptions, IPermissions, IResources, IRoles, IUserId, IUserRoles, IRole, IParents, IResource } from "../interfaces/IAcl";
 import * as _ from "lodash";
 import { IStore } from "../interfaces/IStore";
 import { Common } from "./common";
 
 export class Acl extends Common {
-  readonly options: IOptions;
-  readonly store: IStore;
+  public options: IOptions;
+  public store: IStore;
 
   constructor(store: IStore, options?: IOptions) {
     super();
@@ -49,5 +49,83 @@ export class Acl extends Common {
     });
 
     return this.store.end();
+  }
+
+  async addUserRoles(userId: IUserId, roles: IRoles): Promise<void> {
+    this.store.begin();
+
+    const rolesParams = await Common.makeArray(roles);
+    await this.store.add(this.options.buckets.meta, 'users', userId);
+    await this.store.add(this.options.buckets.users, userId, roles);
+
+    const promises = rolesParams.map((role) => this.store.add(this.options.buckets.roles, role, userId), this);
+    await Promise.all(promises);
+
+    return this.store.end();
+  }
+
+  async userRoles(userId: IUserId): Promise<IUserRoles> {
+    return this.store.get(this.options.buckets.users, userId);
+  }
+
+  async hasRole(userId: IUserId, rolename: IRole): Promise<boolean> {
+    const roles = await this.userRoles(userId);
+
+    return roles.indexOf(rolename as string) !== -1;
+  }
+
+  async roleUsers(roleName: IRole): Promise<[IUserId]> {
+    return this.store.get(this.options.buckets.roles, roleName);
+  }
+
+  async addRoleParents(role: IRole, parents: IParents): Promise<void> {
+    this.store.begin();
+    await this.store.add(this.options.buckets.meta, 'roles', role);
+    await this.store.add(this.options.buckets.parents, role, parents);
+
+    return this.store.end();
+  }
+
+
+
+
+
+  async isAllowed(userId: IUserId, resource: IResource, permissions: IPermissions): Promise<boolean> {
+    const roles = await this.store.get(this.options.buckets.users, userId);
+
+    if (roles.length) {
+      return await this.areAnyRolesAllowed(roles, resource, permissions);
+    }
+
+    return false;
+  }
+
+  async areAnyRolesAllowed(roles: IRoles, resource: IResource, permissions: IPermissions): Promise<boolean> {
+    const [rolesParam, permissionsParam] = await Promise.all([
+      Common.makeArray(roles),
+      Common.makeArray(permissions),
+    ]);
+
+    if (!rolesParam.length) {
+      return false;
+    }
+    return this.checkPermissions(rolesParam, resource, permissionsParam);
+  }
+
+  async checkPermissions(roles, resource, permissions): Promise<boolean> {
+    const resourcePermissions = await this.store.union(this.allowsBucket(resource), roles);
+
+    if (resourcePermissions.indexOf('*') !== -1) {
+      return true;
+    }
+
+    const perms = permissions.filter((p) => resourcePermissions.indexOf(p) === -1);
+
+    if (!perms.length) {
+      return true;
+    }
+
+    const parents = await this.store.union(this.options.buckets.parents, roles);
+    return (parents && parents.length) ? await this.checkPermissions(parents, resource, perms) : false;
   }
 }
