21412d50457e17cd9040ada307e75be3bc39642d
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2019-03-21 14:35:14 +0100

    feat(interfaces/IStore.ts) wip

--- src/interfaces/IStore.ts
+++ src/interfaces/IStore.ts
@@ -30,7 +30,7 @@ export interface IStore {
   //  * and returns a mapping of bucket to union.
   //  * @return Promise<any>
   //  */
-  // unions(bucket: Array<any>, keys: Array<any>): Promise<any>;
+  unions(bucket: Array<any>, keys: Array<any>): Promise<any>;
   //
   // /**
   //  * @description Gets union of contents of the specified keys in each of the specified buckets
@@ -44,17 +44,17 @@ export interface IStore {
   //  * @return Promise<any>
   //  */
   add(bucket: string, key: string | number, values: string | number | Array<any>): any;
-  //
+
   // /**
   //  * @description Deletes given key(s) at the bucket.
   //  * @return Promise<any>
   //  */
-  // del(bucket: string, keys: string | Array<any>): Promise<any>;
-  //
+  del(bucket: string, keys: string | Array<any>): Promise<any>;
+
   // /**
   //  * @description Deletes values from a given key inside a bucket.
   //  * @return Promise<any>
   //  */
-  // remove(bucket: string, key: string | number, values: string | number | Array<any>): Promise<any>;
+  remove(bucket: string, key: string | number, values: string | number | Array<any>): Promise<any>;
 
 }
