3c7d4306edc75e05d24cc52b9c4542970b6ffef1
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2019-03-24 17:44:18 +0100

    feat(types.ts) file renamed + types updated

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,3 @@
-import { IStore } from "./IStore";
-
 export interface IOptions {
   buckets: {
     meta: string;
@@ -11,20 +9,11 @@ export interface IOptions {
   };
 }
 
-export type IRolesObjectAllow = { resources: string; permissions: string; };
-export type IRolesObjectAllows = { resources: string[]; permissions: string[]; };
-
-export type IDemuxed = { roles: IRole | IRoles, resources: IResource | IResources, permissions: IPermission | IPermissions };
-
-export type IRolesObject = { roles: string; allows: IRolesObjectAllow[] | IRolesObjectAllows[]; };
-export type IRolesObjects = IRolesObject[];
-
 export type IRolesParent = string;
 export type IRolesParents = IRolesParent[];
 
 export type IUserId = string | number;
 export type IUserIds = IUserId[];
-
 
 export type IUserRoles = string[];
 
@@ -37,15 +26,15 @@ export type IRoles = IRole[];
 export type IPermission = string;
 export type IPermissions = IPermission[];
 
+export type IRolesObjectAllow = { resources: IResource; permissions: IPermission; };
+export type IRolesObjectAllows = { resources: IResources; permissions: IPermissions; };
+export type IRolesObject = { roles: IRole | IRoles; allows: (IRolesObjectAllow | IRolesObjectAllows)[] };
+export type IRolesObjects = IRolesObject[];
+
 export type IBucket = string
 
+export type IDemuxed = { roles: IRole | IRoles, resources: IResource | IResources, permissions: IPermission | IPermissions };
 
 export interface IDynamicObject {
   [key: string]: any // tslint:disable-line no-any
-}
-
-export interface IAcl {
-  store: IStore;
-  options: IOptions;
-
 }
