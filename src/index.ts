5f9b3edb16e500b20b2285d4802cef0396ab7156
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2019-04-06 18:53:57 +0200

    feat(index.ts) export redis store

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { Acl } from "./classes/acl";
 import { IStore } from "./interfaces/IStore";
 import { MemoryStore } from "./stores/memory";
+import { RedisStore } from "./stores/redis";
 
 export {
   Acl,
   IStore,
   MemoryStore,
+  RedisStore,
 }
