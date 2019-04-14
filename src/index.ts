a1c0d01ff819de3d84d722d79a08bf86e1cd1afe
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2019-04-14 18:30:57 +0200

    feat(stores/index.ts) MongoDBStore imported / exported

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import { Acl } from "./classes/acl";
 import { IStore } from "./interfaces/IStore";
 import { MemoryStore } from "./stores/memory";
 import { RedisStore } from "./stores/redis";
+import { MongoDBStore } from "./stores/mongodb";
 
 export {
   Acl,
   IStore,
   MemoryStore,
   RedisStore,
+  MongoDBStore,
 }
