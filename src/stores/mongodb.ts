1d7b3410243d2945c552c9ac6b39e26456f0266d
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2019-04-20 14:40:55 +0200

    feat(stores/mongodb.ts) adapter created

--- src/stores/mongodb.ts
+++ src/stores/mongodb.ts
@@ -1,109 +1,79 @@
-import async from 'async';
-import _ from 'lodash';
+import * as _ from 'lodash';
+import { IStore } from '..';
+import Bluebird from 'bluebird';
 
-// Name of the collection where meta and allowsXXX are stored.
-// If prefix is specified, it will be prepended to this name, like acl_resources
 const aclCollectionName = 'resources';
 
-export class MongoDB {
+export class MongoDBStore implements IStore {
   private transaction;
-  private db;
-  private prefix;
-  private useSingle;
-  private useRawCollectionNames;
+  private readonly db;
+  private readonly prefix;
+  private readonly useSingle;
+  private readonly useRawCollectionNames;
 
-  constructor(db, prefix, useSingle, useRawCollectionNames) {
+  constructor(db, prefix, useSingle?, useRawCollectionNames?) {
     this.db = db;
     this.prefix = typeof prefix !== 'undefined' ? prefix : '';
     this.useSingle = (typeof useSingle !== 'undefined') ? useSingle : false;
     this.useRawCollectionNames = useRawCollectionNames === false; // requires explicit boolean false value
   }
 
-  /**
-   Begins a transaction.
-   */
-  begin() {
+  public begin(): void {
     this.transaction = [];
   }
 
-  /**
-   Ends a transaction (and executes it)
-   */
-  end() {
-    async.series(this.transaction);
+  public async end(): Promise<void> {
+    await Promise.all(this.transaction.map(async (fn) => {
+      return fn instanceof Promise ? fn.then(() => Promise.resolve()) : Promise.resolve(fn())
+    }));
   }
 
-  /**
-   Cleans the whole storage.
-   */
-  clean() {
-    this.db.collections((_err, collections) => {
-      async.forEach(collections,(coll, innercb) => {
-        coll.drop(() => {innercb()}); // ignores errors
-      });
-    });
+  public async clean(): Promise<void> {
+    const collections = await this.db.collections();
+    // @ts-ignore
+    await Bluebird.map(collections, (collection) => collection.drop());
   }
 
-  /**
-   Gets the contents at the bucket's key.
-   */
-  get(bucket: string, key: string | number): Promise<string[]> {
+  public async get(bucket: string, key: string | number): Promise<string[]> {
     key = encodeText(key);
     const searchParams = (this.useSingle? {_bucketname: bucket, key} : {key});
     const collName = (this.useSingle? aclCollectionName : bucket);
 
-    let output;
-    this.db.collection(this.prefix + this.removeUnsupportedChar(collName),(_err, collection) => {
-      // if(err instanceof Error) return cb(err);
-      // Excluding bucket field from search result
-      collection.findOne(searchParams, {_bucketname: 0}, (_err, doc) => {
-        // if(err) return cb(err);
-        if(! _.isObject(doc) ) {
-          // return cb(undefined,[]);
-          return output = [];
-        }
-        doc = fixKeys(doc);
-        return output = _.without(_.keys(doc),"key","_id");
-      });
-    });
-    return output;
+    const collection = await this.db.collection(this.prefix + this.removeUnsupportedChar(collName))
+
+
+    const document = await collection.findOne(searchParams, {_bucketname: 0});
+
+    if(! _.isObject(document) ) {
+      // @ts-ignore
+      return [];
+    }
+
+    // @ts-ignore
+    return _.without(_.keys(fixKeys(document)),"key","_id");
   }
 
-  /**
-   Returns the union of the values in the given keys.
-   */
-  union(bucket: string, keys: string[]): Promise<string[]> {
+  public async union(bucket: string, keys: string[]): Promise<string[]> {
     keys = encodeAll(keys);
     const searchParams = (this.useSingle? {_bucketname: bucket, key: { $in: keys }} : {key: { $in: keys }});
     const collName = (this.useSingle? aclCollectionName : bucket);
 
-    let output;
-    this.db.collection(this.prefix + this.removeUnsupportedChar(collName),(_err, collection) => {
-      // if(err instanceof Error) return cb(err);
-      // Excluding bucket field from search result
-      collection.find(searchParams, {_bucketname: 0}).toArray((_err, docs) => {
-        // if(err instanceof Error) return cb(err);
-        if( ! docs.length ) {
-          // return cb(undefined, []);
-          return output = [];
-        }
+    const collection = await this.db.collection(this.prefix + this.removeUnsupportedChar(collName));
+    let docs = await collection.find(searchParams, {_bucketname: 0}).toArray();
+    if( ! docs.length ) {
+      return [];
+    }
 
-        const keyArrays = [];
-        docs = fixAllKeys(docs);
-        docs.forEach(doc => {
-          keyArrays.push.apply(keyArrays, _.keys(doc));
-        });
-        return output = _.without(_.union(keyArrays),"key","_id");
-        // cb(undefined, _.without(_.union(keyArrays),"key","_id"));
-      });
+    const keyArrays = [];
+    docs = fixAllKeys(docs);
+    docs.forEach(doc => {
+      keyArrays.push.apply(keyArrays, _.keys(doc));
     });
-    return output;
+
+    return _.without(_.union(keyArrays),"key","_id");
   }
 
-  /**
-   Adds values to a given key inside a bucket.
-   */
-  add(bucket: string, key: string | number, values: number | number[] | string | string[]): void {
+  public add(bucket: string, key: string | number, values: number | number[] | string | string[]): void {
     if(key === "key") {
       throw new Error("Key name 'key' is not allowed.");
     }
@@ -111,92 +81,69 @@ export class MongoDB {
     const self=this;
     const updateParams = (self.useSingle? {_bucketname: bucket, key} : {key});
     const collName = (self.useSingle? aclCollectionName : bucket);
-    this.transaction.push(cb => {
-      values = makeArray(values);
-      self.db.collection(self.prefix + self.removeUnsupportedChar(collName), (err, collection) => {
-        if(err instanceof Error) return cb(err);
 
-        // build doc from array values
-        const doc = {};
-        // @ts-ignore
-        values.forEach(value => {doc[value]=true;});
+    this.transaction.push(async () => {
+      const valuesParam = makeArray(values);
 
-        // update document
-        collection.update(updateParams,{$set:doc},{safe:true,upsert:true}, err => {
-          if(err instanceof Error) return cb(err);
-          cb(undefined);
-        });
-      });
+      const collection = await self.db.collection(self.prefix + self.removeUnsupportedChar(collName));
+      // build doc from array values
+      const doc = {};
+      // @ts-ignore
+      valuesParam.forEach(value => {doc[value]=true;});
+
+      // update document
+      await collection.update(updateParams,{$set:doc},{safe:true,upsert:true});
     });
 
-    this.transaction.push(cb => {
-      self.db.collection(self.prefix + self.removeUnsupportedChar(collName), (_err, collection) => {
-        // Create index
-        collection.ensureIndex({_bucketname: 1, key: 1}, err => {
-          if (err instanceof Error) {
-            return cb(err);
-          } else{
-            cb(undefined);
-          }
-        });
-      });
+    this.transaction.push(async () => {
+      const collection = await self.db.collection(self.prefix + self.removeUnsupportedChar(collName));
+      // Create index
+      await collection.ensureIndex({_bucketname: 1, key: 1});
     })
   }
 
-  /**
-   Delete the given key(s) at the bucket
-   */
-  async del(bucket: string, keys: string | string[]): Promise<void> {
+  public async del(bucket: string, keys: string | string[]): Promise<void> {
     keys = makeArray(keys);
     const self= this;
     const updateParams = (self.useSingle? {_bucketname: bucket, key:{$in:keys}} : {key:{$in:keys}});
     const collName = (self.useSingle? aclCollectionName : bucket);
 
-    this.transaction.push(cb => {
-      self.db.collection(self.prefix + self.removeUnsupportedChar(collName),(err, collection) => {
-        if(err instanceof Error) return cb(err);
-        collection.remove(updateParams,{safe:true},err => {
-          if(err instanceof Error) return cb(err);
-          cb(undefined);
-        });
-      });
+    this.transaction.push(async () => {
+      const collection = await self.db.collection(self.prefix + self.removeUnsupportedChar(collName));
+      await collection.remove(updateParams,{safe:true});
     });
   }
 
-  /**
-   Removes values from a given key inside a bucket.
-   */
-  async remove(bucket: string, key: string | number, values: number | number[] | string | string[]): Promise<void> {
+  public async remove(bucket: string, key: string | number, values: number | number[] | string | string[]): Promise<void> {
     key = encodeText(key);
     const self=this;
     const updateParams = (self.useSingle? {_bucketname: bucket, key} : {key});
     const collName = (self.useSingle? aclCollectionName : bucket);
 
     values = makeArray(values);
-    this.transaction.push(cb => {
-      self.db.collection(self.prefix + self.removeUnsupportedChar(collName),(err, collection) => {
-        if(err instanceof Error) return cb(err);
+    this.transaction.push(async () => {
+      const collection = await self.db.collection(self.prefix + self.removeUnsupportedChar(collName));
+      // build doc from array values
+      const doc = {};
+      // @ts-ignore
+      values.forEach(value => {doc[value]=true;});
 
-        // build doc from array values
-        const doc = {};
-        // @ts-ignore
-        values.forEach(value => {doc[value]=true;});
-
-        // update document
-        collection.update(updateParams,{$unset:doc},{safe:true,upsert:true},err => {
-          if(err instanceof Error) return cb(err);
-          cb(undefined);
-        });
-      });
+      // update document
+      await collection.update(updateParams,{$unset:doc},{safe:true,upsert:true});
     });
   }
 
-  removeUnsupportedChar(text: string) {
+  public removeUnsupportedChar(text: string) {
     if (!this.useRawCollectionNames) {
       text = decodeURIComponent(text);
       text = text.replace(/[/\s]/g, '_'); // replaces slashes and spaces
     }
+
     return text;
+  }
+
+  public async close(): Promise<void> {
+    await this.db.close();
   }
 }
 
@@ -205,6 +152,7 @@ function encodeText(text: string | number) {
     text = encodeURIComponent(text);
     text = text.replace(/\./g, '%2E');
   }
+
   return text;
 }
 
@@ -212,6 +160,7 @@ function decodeText(text: string) {
   if (typeof text == 'string') {
     text = decodeURIComponent(text);
   }
+
   return text;
 }
 
@@ -221,23 +170,12 @@ function encodeAll(arr) {
     arr.forEach(aval => {
       ret.push(encodeText(aval));
     });
+
     return ret;
   } else {
     return arr;
   }
 }
-
-// function decodeAll(arr) {
-//   if (Array.isArray(arr)) {
-//     const ret = [];
-//     arr.forEach(aval => {
-//       ret.push(decodeText(aval));
-//     });
-//     return ret;
-//   } else {
-//     return arr;
-//   }
-// }
 
 function fixKeys(doc) {
   if (doc) {
@@ -247,6 +185,7 @@ function fixKeys(doc) {
         ret[decodeText(key)] = doc[key];
       }
     }
+
     return ret;
   } else {
     return doc;
@@ -259,6 +198,7 @@ function fixAllKeys(docs) {
     docs.forEach(adoc => {
       ret.push(fixKeys(adoc));
     });
+
     return ret;
   } else {
     return docs;
@@ -268,5 +208,3 @@ function fixAllKeys(docs) {
 function makeArray(arr){
   return Array.isArray(arr) ? encodeAll(arr) : [encodeText(arr)];
 }
-
-exports = module.exports = MongoDB;
