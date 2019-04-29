e9fa0cfe701a400db190b2255ea31f8152951a57
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2019-04-29 16:15:13 +0200

    feat(index.ts) errorHandler exported

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import { Acl } from "./classes/acl";
-import { IStore } from "./interfaces/IStore";
-import { MemoryStore } from "./stores/memory";
-import { MongoDBStore } from "./stores/mongodb";
-import { RedisStore } from "./stores/redis";
+import { Acl } from './classes/acl';
+import { errorHandler } from './classes/errorHandler';
+import { HttpError } from './classes/httpError';
+import { IStore } from './interfaces/IStore';
+import { MemoryStore } from './stores/memory';
+import { MongoDBStore } from './stores/mongodb';
+import { RedisStore } from './stores/redis';
 
 export {
   Acl,
@@ -10,4 +12,6 @@ export {
   MemoryStore,
   RedisStore,
   MongoDBStore,
+  HttpError,
+  errorHandler,
 }
